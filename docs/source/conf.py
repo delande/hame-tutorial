7636534ba9fd2e7fc3fd787d6f04a3548ad5f49d
Dominique Delande <Dominique.Delande@lkb.upmc.fr>
2023-03-08 11:27:32 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,11 @@
 """Sphinx configuration."""
 import os
 import sys
-import pyamg
+
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
+
+import pyamg
 
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
