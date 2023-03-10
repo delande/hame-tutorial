86d3bc22e02f899b112e9327cf20ce191ad0f5dc
Dominique Delande <Dominique.Delande@lkb.upmc.fr>
2023-03-10 16:15:02 +0100

    Update hame.py

--- hame.py
+++ hame.py
@@ -22,16 +22,15 @@ __version__ = "1.0"
 # hame.py
 # Author: Dominique Delande
 # Release date: February, 24, 2023
-
 """
-  This program computes one-photon and two-photon matrix elements between hydrogenic states.
+This program computes one-photon and two-photon matrix elements between hydrogenic states.
 
-  It includes diagonal elements of the two-photon matrix elements, that is light-shift and one-photon ionization rate.
+It includes diagonal elements of the two-photon matrix elements, that is light-shift and one-photon ionization rate.
 
-  It performs the numerical calculation in the length gauge in a Sturmian basis.
-  It additionally performs the calculation in the velocity gauge.
+It performs the numerical calculation in the length gauge in a Sturmian basis.
+It additionally performs the calculation in the velocity gauge.
 
-  It checks that results in length and velocity gauges are equal.
+It checks that results in length and velocity gauges are equal.
 """
 
 import numpy as np
